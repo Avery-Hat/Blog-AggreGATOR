a550dc62cc345d291747e9541b18587992d979fa
Avery-Hat <biskatarius@gmail.com>
2026-01-03 01:23:35 -0800

    chapter 2 parts 1 - 3

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
-	"os"
-
 	"gator/internal/config"
+	"gator/internal/database"
+	"log"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 type command struct {
@@ -33,12 +42,54 @@ func (c *commands) run(s *state, cmd command) error {
 	return handler(s, cmd)
 }
 
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return errors.New("register requires a username")
+	}
+
+	name := cmd.args[0]
+	now := time.Now()
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	})
+	if err != nil {
+		// Unique violation: name already exists -> exit(1)
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			return fmt.Errorf("user %s already exists", name)
+		}
+		return err
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
+		return err
+	}
+
+	fmt.Printf("user %s created\n", name)
+	log.Printf("created user: %+v\n", user)
+	return nil
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 {
 		return errors.New("login requires a username")
 	}
 
 	username := cmd.args[0]
+
+	// Must error if user doesn't exist
+	_, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		// sqlc usually returns sql.ErrNoRows when not found
+		if errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("user %s does not exist", username)
+		}
+		return err
+	}
+
 	if err := s.cfg.SetUser(username); err != nil {
 		return err
 	}
@@ -61,12 +112,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	s := &state{cfg: &cfg}
+	// Open DB connection
+	dbURL := cfg.DBURL // if your field name differs, change this line to match
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
+	s := &state{
+		cfg: &cfg,
+		db:  dbQueries,
+	}
 
 	cmds := &commands{
 		handlers: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	cmdName := os.Args[1]
 	cmdArgs := os.Args[2:]
