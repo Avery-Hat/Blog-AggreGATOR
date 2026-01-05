3d1260d790dcb7dabbb2c550c32824b10f77009d
Avery-Hat <biskatarius@gmail.com>
2026-01-05 10:58:48 -0800

    added chapter 5, finished process. updating readme next

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,15 @@ ORDER BY feeds.created_at ASC;
 -- name: GetFeedByURL :one
 SELECT * FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(),
+    updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST, created_at ASC
+LIMIT 1;
